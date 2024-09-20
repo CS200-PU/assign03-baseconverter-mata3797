579efb5e2fab9a82831a15886e422400f7e595c0
mata3797 <112443798+mata3797@users.noreply.github.com>
2024-09-20 21:22:13 +0000

    setting up github codespaces

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
+// Author:      Keolakupono Mataele
+// Date:        09/23/2024
+// Class:       CS200
+// Assignment:  03 Base Converter
+// Purpose:     Convert between base-10, binary and hexidecimal numbers
 // Hours:       0.25
 //******************************************************************************
 
